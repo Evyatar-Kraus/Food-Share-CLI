519b182b523166a9d384eb51b6b0fbcf2bf6c88f
Evyatar <29202833+Evyatar-Kraus@users.noreply.github.com>
2021-11-22 17:30:18 +0200

    save works again

--- faker_data.py
+++ faker_data.py
@@ -1,41 +1,62 @@
+from shared_food import SharedFood
 from faker import Faker
+# from faker import providers.address
+from pprint import pprint
+
 fake = Faker()
 
 food_names_list = [
-'danish','cheesecake','sugar',
-'Lollipop','wafer','Gummies',
-'sesame','Jelly','beans',
-'pie','bar','Ice','oat' ]
+    'danish', 'cheesecake', 'sugar',
+    'Lollipop', 'wafer', 'Gummies',
+    'sesame', 'beans',
+    'pie', 'bar', 'Ice', 'oat']
 
-first_name_list = ['Dani','Daniel','Alice','Johnny','Yogev','Sarah']
-last_name_list = ['Coh','Daniel','Alice','Johnny','Yogev','Sarah']
+first_name_list = ['Dani', 'Daniel', 'Alice', 'Johnny', 'Yogev', 'Sarah']
+last_name_list = ['Coh', 'Daniel', 'Alice', 'Johnny', 'Yogev', 'Sarah']
+# building_number = range(0,21)
+# street_name = []
+# city = []
+# country = []
+fake_address  = "herzl 8 tel aviv israel"
 
-
-def faker_user_generate(type=None):
-    types = {
-        'user_id':fake.pyint(min_value=1,max_value=500),
-        'food_title':fake.word(ext_word_list=food_names_list).capitalize(),
-        'food_text':' '.join(fake.sentence(ext_word_list=food_names_list).split(' ')[0:4]*4),
-        'address': f"{fake.street_address()}, {fake.city()}, {fake.country()}",
-        # 'first_name':fake.first_name(),
-        # 'last_name':fake.last_name(),
-        'created_date':fake.date(),
-        'updated_date':fake.date(),
-        'contact_phone':fake.phone_number(),
-        'published':fake.boolean()
+def faker_shared_food_generate(type=None, faker_mode=False):
+    shared_food_attributes = {
+        'food_title': fake.word(ext_word_list=food_names_list).capitalize(),
+        'food_text': ' '.join(fake.sentence(ext_word_list=food_names_list).split(' ')[0:4]),
+        # 'food_text':"'Cheesecake with a sprinkle of sugar'",
+        'address': fake_address or f"{fake.street_address()}, {fake.city()}, {fake.country()}",
+        'first_name': fake.first_name(),
+        'last_name': fake.last_name(),
+        'contact_phone': fake.phone_number(),
     }
 
-    return types.get(type) if type else types
+    faked_attrs = {
+            'user_id': fake.pyint(min_value=1, max_value=500),
+            'created_at': fake.date(),
+            'updated_at': fake.date(),
+            'published': fake.boolean(),
+    }
+
+    if faker_mode:
+        shared_food_attributes.update(faked_attrs)
+
+    return shared_food_attributes.get(type) or faked_attrs.get(type) if type else shared_food_attributes
 
 
-print(faker_user_generate())
-print(faker_user_generate('user_id'))
-print(faker_user_generate('food_title'))
+# print(faker_user_generate())
+# print(faker_user_generate('user_id'))
+# print(faker_user_generate('food_title'))
 # print(faker_user_generate('food_text'))
 # print(faker_user_generate('address'))
 # print(faker_user_generate('first_name'))
 # print(faker_user_generate('last_name'))
 # print(faker_user_generate('created_date'))
-# print(faker_user_generate('updated_date'))
+# print(faker_user_generate('updated_at'))
 # print(faker_user_generate('published'))
 # print(faker_user_generate('contact_phone'))
+
+pprint(faker_shared_food_generate()['food_text'])
+new_food_share = SharedFood(faker_shared_food_generate())
+# print(vars(new_food_share))
+# print(new_food_share.user_id)
+# new_food_share.to_json()
--- shared_food.py
+++ shared_food.py
@@ -9,68 +9,179 @@
 #   update - change details - name or address
 #   remove - delete itself
 
+from logging import PlaceHolder
+import psycopg2
 from psycopg2 import sql
-# from utils import run_query
+from utils import run_query, get_distance
 from datetime import datetime
+from pprint import pprint
+import itertools
+from location_api import address_to_coords
 
-__TABLE_NAME = 'shared_foods'
+
+
+#table create script
+# CREATE TABLE shared_foods
+#  (
+#      shared_food_id serial primary key,
+#      food_title varchar(100) NOT NULL,
+#      food_text text NOT NULL,
+#      first_name varchar(50) NOT NULL,
+#      last_name varchar(50) NOT NULL,
+#      user_id INTEGER NOT NULL,
+#      address point NOT NULL, -- as coordinates
+#      contact_phone varchar(20) NOT NULL
+#      created_at DATE NOT NULL DEFAULT CURRENT_DATE,
+#      updated_at DATE NOT NULL DEFAULT CURRENT_DATE,
+#      published BOOLEAN NOT NULL DEFAULT FALSE ,
+#      FOREIGN key(user_id) references user(user_id)
+# );
+
+__TABLE_NAME ='shared_foods'
+
+_GIVEN_ATTRIBUTES = ['first_name', 'last_name', 'food_title',
+                    'food_text', 'contact_phone', 'address'] #given when calling the class
+_SELF_GENERATED_ATTRIBUTES = ['updated_at','created_at','published','lon','lat'] #generated in __init__
+_DB_GENERATED_ATTRIBUTES = ['shared_food_id']
 
 class SharedFood:
-    def __init__(self,**kwargs) -> None:
-        #given when calling the class
-        _VALID_KEYWORDS = {'user_id','food_title','food_text','contact_phone','address'}
+    #class attributes
+    format_code = '%Y/%m/%d'
+    _TABLE_NAME = 'shared_foods'
+    #column list for row that represent the invoked instace in the database table
+    column_list = list(itertools.chain(_GIVEN_ATTRIBUTES,_SELF_GENERATED_ATTRIBUTES))
+
+    def __init__(self, kwargs):
+        # given when calling the class
         for keyword, value in kwargs.items():
-            if keyword in self._VALID_KEYWORDS:
+            if keyword in _GIVEN_ATTRIBUTES:
                 setattr(self, keyword, value)
             else:
                 raise ValueError(
                     "Unknown keyword argument: {!r}".format(keyword))
-        self.created_date = datetime.datetime.now()
-        self.updated_date = datetime.datetime.now()
-        self.published = False
-        self.save()
 
-    def modify(self, **kwargs):
-        #change details based on a passed dictionary
-        pass
+        self.lat = None
+        self.lon = None
+        self.coordinates = {'lat':None,'lon':None}
+
+        try:
+            self.coordinates = address_to_coords(self.address)
+            self.lat = self.coordinates['lat']
+            self.lon = self.coordinates['lon']
+        except:
+            print("Error getting address coordinates - coordinates will not be saved")
+
+        self.created_at = datetime.strftime(
+            datetime.now(), self.format_code)
+        self.updated_at = datetime.strftime(
+            datetime.now(), self.format_code)
+        self.published = str(False)
+        shared_food_id = self.save()
+
+    # def modify(self, **kwargs):
+    #     #change details based on a passed dictionary
+    #     pass
 
     def save(self):
-        #insert to database here
-        pass
+        print("save - insert to db here")
+
+        column_list = self.column_list
+        value_list = [getattr(self,key) for key in self.column_list]
+        pprint(column_list)
+        pprint(value_list)
+        col_names = sql.SQL(', ').join(sql.Identifier(n) for n in column_list )
+        values = sql.SQL(', ').join(sql.Literal(n) for n in value_list )
+
+        # query = sql.SQL("INSERT INTO {} ({}) VALUES ({}) ON CONFLICT DO NOTHING RETURNING shared_food_id").format(
+            # table_name =sql.Identifier(self._TABLE_NAME),  # table name
+            # col_names = col_names,
+            # values = values
+        # )
+
+        # query = f"INSERT INTO {self._TABLE_NAME} ({ ', '.join(column_list) }) VALUES ({', '.join(value_list)}) \
+        #     ON CONFLICT DO NOTHING RETURNING shared_food_id;"
+        print()
+        print(len(column_list))
+        print(len(value_list))
+        query = sql.SQL("insert into {} ({col_names}) values ({values})").format(
+            sql.Identifier(self._TABLE_NAME),
+            col_names = sql.SQL(', ').join(sql.Identifier(n) for n in column_list),
+             values = sql.SQL(', ').join(sql.Literal(n) for n in value_list )
+            )
+        print(query)
+        result = run_query(query,mode='w')
+        print(result)
+
 
     def publish(self):
-        #change this one's published status to True
-        pass
+        # change this one's published status to True
+        try:
+            query =  sql.SQL("update {} set published = true where shared_food_id = %s;") .format(
+            sql.Identifier(__TABLE_NAME)),  [self.shared_food_id]
+            run_query(query)
+            self.published = True
+        except:
+            print("Error publishing this food")
+            pass
 
     def unpublish(self):
-        #change this one's published status to False
-        pass
+        # change this one's published status to False
+        try:
+            query =  sql.SQL("update {} set published = false where shared_food_id = %s;") .format(
+            sql.Identifier(__TABLE_NAME)),  [self.shared_food_id]
+            run_query(query)
+            self.published = False
+        except:
+            print("Error unpublishing this food")
+            pass
 
-    def distance_from(self,user_id=None,coordinates={}):
-        #get distance from user - or from coordinates
+    def distance_by_km_from_address(self,address):
+        # get distance from user - or from coordinates
+        distance = get_distance(address_to_coords(address))
+        # SELECT ST_DistanceSphere(ST_MakePoint(103.776047, 1.292149),ST_MakePoint(103.77607, 1.292212));
+        return distance
         pass
 
     def to_json(self):
-        data = {
-            'first_name':self.first_name,
-            'last_name':self.last_name,
-            'food_name':self.food_name,
-            'food_text':self.food_text,
-            'food_address':self.food_address,
-            'published':self.published,
-            'contact_phone':self.contact_phone,
-            'created_date':self.created_date,
-            'updated_date':self.updated_date
-        }
+        # print('to_json')
+        data = vars(self)
+        data['created_at'] = datetime.strftime(
+            data['created_at'], self.format_code)
+        data['updated_at'] = datetime.strftime(
+            data['updated_at'], self.format_code)
+        # print(data)
+        # data = {
+        #     'first_name':self.first_name,
+        #     'last_name':self.last_name,
+        #     'food_name':self.food_name,
+        #     'food_text':self.food_text,
+        #     'food_address':self.food_address,
+        #     'published':self.published,
+        #     'contact_phone':self.contact_phone,
+        #     'created_at':self.created_at,
+        #     'updated_at':self.updated_at
+        # }
         return data
 
-    def delete(self):
-        #remove this sharing from the database
+    @classmethod
+    def from_json(cls):
         pass
 
-    def save(self):
-        # query = f"INSERT INTO student (first_name, last_name, class_id) VALUES \
-        # ('{self.first_name}', '{self.last_name}', '{self.classroom.id}')"
-        # query = sql.SQL("insert into {} values (%s, %s)") .format(sql.Identifier('my_table')),  [10, 20]
-        # run_query(query)
+    def delete(self, id):
+        # remove this sharing from the database
+        query = f'DELETE from {__TABLE_NAME} where shared_food_id = {id};'
+        query = sql.SQL("insert into {} values (%s, %s)") .format(
+            sql.Identifier(__TABLE_NAME)),  [self.created_date]
+        print("deleted")
         pass
+
+    def date_str(self):
+        return self.date.strftime(self.format_code)
+
+    def __str__(self):
+        pass
+        # return f'{self.date_str()}'
+
+    def __repr__(self):
+        pass
+        # return self.__str__()
