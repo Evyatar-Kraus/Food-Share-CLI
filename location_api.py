17f58c66a94b4965d2ffc32db94b74ffb872bee1
Evyatar <29202833+Evyatar-Kraus@users.noreply.github.com>
2021-11-24 01:19:34 +0200

    get coordinates - structured url query

--- location_api.py
+++ location_api.py
@@ -7,20 +7,15 @@ location_api_url = 'https://nominatim.openstreetmap.org/search.php'
 
 def address_to_coords(num,st,city,country):
     try:
-        # address = '+'.join(address.split(' '))
-        # street = f'{street_num}/{street_name}
         st = '+'.join(st.split(' '))
         city = '+'.join(city.split(' '))
         country = '+'.join(country.split(' '))
-
         address = f"street={num}+%2F+{st}&city={city}+&country={country}"
-        print(address)
         url = f'{location_api_url}?{address}&format=jsonv2'
         response = requests.get(url)
         coords = {'lat':response.json()[0]['lat'],'lon':response.json()[0]['lon']}
-        print(coords)
         return coords;
     except:
-        raise Exception("error getting coords from address")
+        raise Exception("error getting coordinates from address")
 
-# address_to_coords("herzl 8 tel aviv israel")
+# print(address_to_coords(8,"herzl","tel aviv","israel"))
