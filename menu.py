b436186685e065d70b41cf9908b9975f256c582d
Evyatar <29202833+Evyatar-Kraus@users.noreply.github.com>
2021-11-24 01:38:11 +0200

    env example

--- menu.py
+++ menu.py
@@ -10,35 +10,29 @@ def show_user_menu():
     (s) Search foods near you
     (a) Add a food
     (d) Delete a food
-    (v) View all the foods
+    (g) Show specific food
     (x) Exit
     ''')
     return user_input
 
-
-def load_manager(*args):
-    return SharedFood(*args)
-
-
 def add_food():
-    print("Adding a new item to the menu:\n")
+    print("Adding a new food:\n")
     shared_food_attributes = {}
     for att in _GIVEN_ATTRIBUTES:
         shared_food_attributes[att] = input(f"What is the {att}?\n")
-    new_food_to_add = load_manager(shared_food_attributes)
     try:
-        # new_item_to_add.save()
+        new_food_to_add = SharedFood(shared_food_attributes)
+        print(new_food_to_add.get_created_at())
         print("item was added successfully.\n")
     except:
         print("Error adding item.")
 
 
 def remove_food():
-    item_to_remove_input = input("What item do you want to remove?\n")
+    food_to_remove_id_input = input("What item do you want to remove? please enter the id:\n")
     try:
-        menu_item = SharedFood.get_by_name(item_to_remove_input)
-        print(menu_item)
-        menu_item.delete()
+        food = SharedFood.get_by_id(int(food_to_remove_id_input))
+        food.delete()
         print("Item deleted successfully")
     except:
         print("Error - item was not deleted")
@@ -47,15 +41,30 @@ def remove_food():
 #TODO need to sort these by location
 #so it will show the closest first etc...
 def show_foods(num,st,city,country):
-    #show all items in menu
-    print(f"{num} {st} {city} {country}")
     foods = SharedFood.all()
     distances = {}
     for food in foods:
         distances[food.shared_food_id] = food.distance_by_km_from_address(num,st,city,country)
     for item in foods:
-        print(item.city)
-        print(item.street_name)
         print(f"""{item}
-        distance: {distances[item.shared_food_id]:.2f}km
+distance: {distances[item.shared_food_id]:.2f}km
         """)
+
+def get_specific_food():
+        food_id = input("what is the food id?\n")
+        food = None
+        while not food:
+            try:
+                food = SharedFood.get_by_id(int(food_id))
+                print(food)
+                print(food.get_created_at())
+            except:
+                print("error - maybe check if the id is valid?")
+            finally:
+                continue
+
+def show_start_greeting():
+    print("Welcome to food share!")
+    print("""Here you can share food you don't want with your neighbors!
+Or get something tasty to eat... and avoid waste all together!
+\n""")
